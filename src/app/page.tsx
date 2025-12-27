f6bff1a3c6aba6210d26a0e9b45332d2023fa235
MARIESWARAN B <96181603+Marieswaran25@users.noreply.github.com>
2025-12-27 19:37:30 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,23 +28,7 @@ export default function HomePage() {
       width="100%"
     >
     </iframe>
-             <Script           strategy="beforeInteractive">
-      const youriframeurl = "https://form.elatrus.com";
-      console.log("youriframeurl", youriframeurl);
-      function receiveMessage(event) {
-        const origin = event.origin || "";
-        console.log("origin", event);
-        console.log("received message", event.data);
-        if (event.data.type === "setHeight") {
-          const iFrameID = document.getElementById("idIframe");
-          if (iFrameID) {
-            iFrameID.style.height = `${event.data.height}px`;
-          }
-        }
-      }
-
-      window.addEventListener("message", receiveMessage, false);
-    </Script>
+            
         </main>
     );
 }
